5190243999619de52f4cb2017d0a0b79c95a2b73
Jonny Smith <jdasmith@gmail.com>
2021-12-18 19:23:11 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+bluetooth.onBluetoothConnected(function () {
+    connected = true
+    basic.showIcon(IconNames.Yes)
+})
+bluetooth.onBluetoothDisconnected(function () {
+    connected = false
+    basic.showIcon(IconNames.No)
+})
+let hb = ""
+let temp = 0
+let counter = 0
+let connected = false
+bluetooth.startUartService()
+basic.showIcon(IconNames.SmallSquare)
+basic.forever(function () {
+    if (connected == true) {
+        bluetooth.uartWriteString("Test:" + counter)
+        bluetooth.uartWriteString("Temp" + temp)
+    }
+})
+basic.forever(function () {
+    basic.pause(100)
+    counter += 1
+    temp = input.temperature()
+})
+basic.forever(function () {
+    hb = bluetooth.uartReadUntil(serial.delimiters(Delimiters.Fullstop))
+    if (hb == "reset") {
+        control.reset()
+    }
+})
